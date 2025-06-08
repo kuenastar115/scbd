c5d48bafe2d737970f26dffdf9a5e5edbf6b63b0
kuenastar115 <kuenastar115@gmail.com>
2025-06-08 12:22:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -109,11 +109,10 @@ if (document.getElementById('title-section')) {
 
             descEl.innerHTML = `
               <p class="description">
-                This document is entitled <strong>${doc.Title}</strong> uploaded by Scribd Download Team.
-                It contains ${doc.Pages} pages in a PDF document type. This document with ID ${doc.ID}
-                has been downloaded for ${doc.Views} times. The information we can get from
-                ${doc.Title} includes ${doc.Summary}.
+                <strong>${doc.Title}</strong> contains ${doc.Pages} pages in a PDF document type uploaded by SCRB Downloader Team. This PDF document with an ID ${doc.ID}
+                has been downloaded for ${doc.Views} times.
               </p>
+              <p class="description">In this document entitled ${doc.Title}, we can get a lot of benefits. ${doc.Summary}.</p>
               <a class="download-button" href="${downloadUrl}" target="_blank"> DOWNLOAD <span style="font-size: 20px;">⬇️</span></a>
             `;
 
